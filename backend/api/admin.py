5a0285a9beaa5fbd630b1faa9aa66df36be325a9
raymond2330 <raymondpostrero1@gmail.com>
2025-05-08 18:29:13 +0800

    update model for research paper

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,8 +4,28 @@ from .models import (
     Category, Keyword, PaperKeyword, DatasetKeyword, PermissionChangeLog
 )
 
+class PaperAuthorInline(admin.TabularInline):
+    model = PaperAuthor
+    extra = 1
+
+class PaperKeywordInline(admin.TabularInline):
+    model = PaperKeyword
+    extra = 1
+
+class ResearchPaperAdmin(admin.ModelAdmin):
+    inlines = [PaperAuthorInline, PaperKeywordInline]
+    list_display = ('title', 'journal', 'publication_date', 'access_setting', 'uploaded_by')
+    search_fields = ('title', 'journal')
+    list_filter = ('access_setting', 'publication_date')
+
+try:
+    admin.site.unregister(ResearchPaper)
+except admin.sites.NotRegistered:
+    pass
+
+admin.site.register(ResearchPaper, ResearchPaperAdmin)
+
 admin.site.register(User)
-admin.site.register(ResearchPaper)
 admin.site.register(Dataset)
 admin.site.register(Request)
 admin.site.register(Author)
@@ -13,6 +33,5 @@ admin.site.register(PaperAuthor)
 admin.site.register(DatasetAuthor)
 admin.site.register(Category)
 admin.site.register(Keyword)
-admin.site.register(PaperKeyword)
 admin.site.register(DatasetKeyword)
 admin.site.register(PermissionChangeLog)
