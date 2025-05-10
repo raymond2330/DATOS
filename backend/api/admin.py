73541c5c85079dae9d8d3852bd8fb350fb5f0a15
raymond2330 <raymondpostrero1@gmail.com>
2025-05-10 16:34:29 +0800

    refactor admin panel: consolidate dataset and request management classes

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -18,20 +18,57 @@ class ResearchPaperAdmin(admin.ModelAdmin):
     search_fields = ('title', 'journal')
     list_filter = ('access_setting', 'publication_date')
 
+class DatasetAuthorInline(admin.TabularInline):
+    model = DatasetAuthor
+    extra = 1
+
+class DatasetKeywordInline(admin.TabularInline):
+    model = DatasetKeyword
+    extra = 1
+
+class DatasetAdmin(admin.ModelAdmin):
+    inlines = [DatasetAuthorInline, DatasetKeywordInline]
+    list_display = ('title', 'version', 'size', 'access_setting', 'uploaded_by')
+    search_fields = ('title', 'version')
+    list_filter = ('access_setting',)
+
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('user', 'paper', 'dataset', 'purpose', 'status', 'request_date')
+    search_fields = ('user__username', 'purpose')
+    list_filter = ('status', 'request_date')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'affiliation')
+    search_fields = ('name', 'affiliation')
+
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ('term',)
+    search_fields = ('term',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class DatasetAuthorAdmin(admin.ModelAdmin):
+    list_display = ('dataset', 'author')
+    search_fields = ('dataset__title', 'author__name')
+
+class DatasetKeywordAdmin(admin.ModelAdmin):
+    list_display = ('dataset', 'keyword')
+    search_fields = ('dataset__title', 'keyword__term')
+
 try:
     admin.site.unregister(ResearchPaper)
 except admin.sites.NotRegistered:
     pass
 
 admin.site.register(ResearchPaper, ResearchPaperAdmin)
-
+admin.site.register(Dataset, DatasetAdmin)
+admin.site.register(Request, RequestAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Keyword, KeywordAdmin)
 admin.site.register(User)
-admin.site.register(Dataset)
-admin.site.register(Request)
-admin.site.register(Author)
-admin.site.register(PaperAuthor)
-admin.site.register(DatasetAuthor)
-admin.site.register(Category)
-admin.site.register(Keyword)
-admin.site.register(DatasetKeyword)
+admin.site.register(DatasetAuthor, DatasetAuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(DatasetKeyword, DatasetKeywordAdmin)
 admin.site.register(PermissionChangeLog)
