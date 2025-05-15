602e4ffae6bf11890bc3749ed7618b6fd3923623
raymond2330 <raymondpostrero1@gmail.com>
2025-05-15 18:28:39 +0800

    remove unnecessary function

--- backend/datos/urls.py
+++ backend/datos/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import UserCreateView
+# from api.views import UserCreateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api.views import (
     UserListView, ResearchPaperListView, DatasetListView, RequestCreateView,
